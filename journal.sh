b9b01fd209172c175937a938854792eb0340c07d
Joe Westra <joetheguy@msn.com>
2017-10-13 08:04:57 -0700

    added arguments

--- journal.sh
+++ journal.sh
@@ -4,13 +4,30 @@
 TODAY=$(date +%F)
 JOURNAL_DIR=/media/FILES/journal/
 TODAYS_JOURNAL=${JOURNAL_DIR}${TODAY}.jent
-if [ ! -f "$TODAYS_JOURNAL" ]
-then
-  echo $(date +%A%B%d%Y) >> $TODAYS_JOURNAL
-  cat ${JOURNAL_DIR}TEMPLATE.jent >> $TODAYS_JOURNAL
-fi
 
-nano $TODAYS_JOURNAL
-echo "${TODAY}'s entry updated."
+openEntry ()
+{
+#place journal opening code here.  Access parameters passed in by $1, $2, etc.
+echo "hello"
+}
+
+#getopts looks for single character options ('-y' in the case below)
+#any option with a following colon looks for an argument after the option
+#and stores it in the variable OPT
+while getopts "y:" OPT; do
+  case $OPT in
+  
+
+  *)
+    if [[ ! -f "$TODAYS_JOURNAL" ]]; then
+      echo $(date +%A%B%d%Y) >> $TODAYS_JOURNAL
+      cat ${JOURNAL_DIR}TEMPLATE.jent >> $TODAYS_JOURNAL
+    fi
+    nano $TODAYS_JOURNAL
+    echo "${TODAY}'s entry updated."
+  ;;
+  esac
+
+done
 
 exit
