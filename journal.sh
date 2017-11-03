5c42b22ffee83510bce0cbecbcb6d1e20a9c54e5
Joe Westra <joetheguy@msn.com>
2017-11-03 00:37:13 -0700

    added option for deletion of today's entry

--- journal.sh
+++ journal.sh
@@ -9,10 +9,21 @@ TODAYS_JOURNAL=${JOURNAL_DIR}${TODAY}.jent
 openEntry () {
     if [[ ! -f "$1" ]]; then
       echo $(date '+%A %B %d %Y') >> $1
-      cat ${JOURNAL_DIR}TEMPLATE.txt >> $1
+  echo "Do you really want to delete today's entry?!?"
+     echo "Yes/No"
+     read
+    cat ${JOURNAL_DIR}TEMPLATE.txt >> $1
     fi
     nano $1
     echo "${TODAY}'s entry updated."
+}
+
+deleteEntry () {
+  echo "Do you really want to delete today's entry?!?"
+  read -p "yes/no:" answer
+  if [[ $answer = 'yes' ]]; then
+    rm $TODAYS_JOURNAL
+  fi
 }
 
 #Prints help text
@@ -22,6 +33,7 @@ help () {
   echo "-h	Display help text"
   echo "-y	Opens yesterday's journal entry.  Numerical arguments"
   echo "	following this option will jump backwards that many days."
+  echo "-d	Deletes todays journal entry after prompting"
 }
 
 
@@ -35,7 +47,7 @@ else
 #any option with a following colon looks for an argument after the option
 #and stores it in the variable OPT
 
-while getopts "thy:" OPT; do
+while getopts "thy:d" OPT; do
   case $OPT in
   t) nano ${JOURNAL_DIR}TEMPLATE.txt
   ;;
@@ -43,6 +55,8 @@ while getopts "thy:" OPT; do
   ;;
   y) PREV_DAY=$(date --date=$OPTARG' days ago' +"%F")
      openEntry ${JOURNAL_DIR}${PREV_DAY}.jent
+  ;;
+  d) deleteEntry
   ;;
   esac
 done
